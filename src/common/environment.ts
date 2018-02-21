6389ffd754c38b7008b50512ee558834cbd84d50
Alexander Tiplea <alexander.tiplea@ing.nl>
2018-02-21 09:42:25 +0100

    Fixes to environment config

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -84,7 +84,8 @@ export async function workspaceQuickPick(): Promise<WorkspaceQuickPick | undefin
 	if (items.length === 1) return items[0];
 	let configureEnvironments = '\u270E Edit Environments...';
 	items.push({ label: configureEnvironments, description: '', fsPath: '' });
-	let choice = await vscode.window.showQuickPick(items, { placeHolder: 'Select a Workspace.' })
+	let choice = await vscode.window.showQuickPick(items, { placeHolder: 'Select a Workspace.' , ignoreFocusOut: true})
+	if (!choice) return;
 	if (choice.label === configureEnvironments) {
 		await GlobalFile.show();
 		return;
@@ -122,9 +123,7 @@ async function environmentQuickPick(workspaceFile: WorkspaceFile) {
 		workspaceEnvironments = await workspaceFile.environment;
 	}
 	catch (e) {
-		if (!workspaceFile.environmentPath) {
-			await workspaceFile.writeLocalEnv({'names': []});
-		}
+		await workspaceFile.writeLocalEnv({'names': []});
 	}
 	let names = workspaceEnvironments.names
 	do {
@@ -140,7 +139,7 @@ async function environmentQuickPick(workspaceFile: WorkspaceFile) {
 		if (vscode.workspace.workspaceFolders.length > 1) {
 			items.push({ label: back, description: '' })
 		}
-		choice = await vscode.window.showQuickPick(items, { placeHolder: `Enable environments for ${workspaceFile.workspaceFolder.name}` });
+		choice = await vscode.window.showQuickPick(items, { placeHolder: `Enable environments for ${workspaceFile.workspaceFolder.name}`, ignoreFocusOut: true });
 		if (choice) {
 			if (choice.label === configureEnvironments) {
 				GlobalFile.show();
@@ -242,12 +241,14 @@ export class WorkspaceFile {
 	 */
 	get environmentObjects(): Promise<EnvironmentConfig[]> {
 		if (this._environmentObjects) return Promise.resolve(this._environmentObjects);
-		return new Promise(async (resolve) => {
-			let environment = await this.environment;
-			let globalEnv = await this.getEnvironmentFromGlobalConfig(environment.names);
-			this._environmentObjects = globalEnv;
-			resolve(this._environmentObjects);
-		})
+		return this.getEnvironmentObjects();
+	}
+
+	private async getEnvironmentObjects() {
+		let environment = await this.environment;
+		let globalEnv = await this.getEnvironmentFromGlobalConfig(environment.names);
+		this._environmentObjects = globalEnv;
+		return this._environmentObjects;
 	}
 
 	/**
