450caebfeb771bff68b2eb204ec0013fb3039e0a
Alexander Tiplea <Alexander.Tiplea@ing.com>
2018-07-30 12:11:34 +0200

    Return proper error code in psl-lint cli

--- src/pslLint/cli/cli.ts
+++ src/pslLint/cli/cli.ts
@@ -8,12 +8,14 @@ import * as api from '../api';
 import { setConfig } from '../config';
 
 async function readFile(filename: string): Promise<number> {
-	if (path.extname(filename) !== '.PROC' && path.extname(filename) !== '.BATCH' && path.extname(filename).toUpperCase() !== '.PSL') return;
+	let errorCount = 0;
+	if (path.extname(filename) !== '.PROC' && path.extname(filename) !== '.BATCH' && path.extname(filename).toUpperCase() !== '.PSL')
+		return errorCount;
 	let filePath = path.join(process.cwd(), filename);
 	let fileBuffer = await fs.readFile(filePath)
 	let textDocument = fileBuffer.toString();
 	let parsedDocument = prepareDocument(textDocument, filePath);
-	let configPath = path.join(process.cwd(),'psl-lint.json');
+	let configPath = path.join(process.cwd(), 'psl-lint.json');
 
 	let useConfig;
 	await fs.lstat(configPath).then(async () => {
@@ -24,14 +26,13 @@ async function readFile(filename: string): Promise<number> {
 	});
 
 	let diagnostics = getDiagnostics(parsedDocument, useConfig);
-	let exitCode = 0;
 	diagnostics.forEach(d => {
 		let range = `${d.range.start.line + 1},${d.range.start.character + 1}`;
 		let severity = `${api.DiagnosticSeverity[d.severity].substr(0, 4).toUpperCase()}`
-		if (d.severity === api.DiagnosticSeverity.Warning || d.severity === api.DiagnosticSeverity.Error) exitCode = 1;
+		if (d.severity === api.DiagnosticSeverity.Warning || d.severity === api.DiagnosticSeverity.Error) errorCount += 1;
 		console.log(`${filename}(${range}) [${severity}][${d.source}] ${d.message}`)
 	})
-	return exitCode;
+	return errorCount;
 }
 
 function prepareDocument(textDocument: string, filename: string): api.PslDocument {
@@ -39,7 +40,7 @@ function prepareDocument(textDocument: string, filename: string): api.PslDocumen
 	return new api.PslDocument(parsedDocument, textDocument, filename);
 }
 
-async function cli(fileString: string) {
+export async function cli(fileString: string) {
 	let files = fileString.split(';');
 	let promises: Promise<any>[] = [];
 	let exitCode = 0;
@@ -54,8 +55,8 @@ async function cli(fileString: string) {
 			})
 		}
 		else if (stat.isFile()) {
-			let promise = readFile(fsPath).then(code => {
-				if (code !== 0) exitCode = code;
+			let promise = readFile(fsPath).then(errorCount => {
+				exitCode += errorCount;
 			}).catch(e => {
 				if (e.message) console.error(fsPath, e.message);
 				else console.error(fsPath, e);
@@ -75,8 +76,10 @@ if (require.main === module) {
 		.parse(process.argv);
 
 	if (commander.args[0]) {
+		console.log('Starting lint.');
 		cli(commander.args[0]).then(exitCode => {
-			process.exit(exitCode)
+			console.log('Finished lint.');
+			process.exit(exitCode);
 		});
 	}
 	else {
