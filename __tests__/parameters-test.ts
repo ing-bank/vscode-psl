37c8a08296d69c76bf301ecfe3c35d306998f052
Alexander Tiplea <Alexander.Tiplea@ing.com>
2018-10-09 16:19:18 +0200

    Add utility module for testing rules

--- __tests__/parameters-test.ts
+++ __tests__/parameters-test.ts
@@ -1,66 +1,44 @@
 import { MethodParametersOnNewLine } from '../src/pslLint/parameters';
 import * as api from '../src/pslLint/api';
-import { parseText } from '../src/parser/parser';
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as activate from '../src/pslLint/activate';
-
-const testFilePath = path.resolve('__tests__', 'files', 'ZTestParams.PROC');
-let parametersReport: api.Diagnostic[];
-
-/**
- * 
- * @param reports The reports to filter
- * @param lineNumber Zero-based line number to check, i.e. line 1 of the document is lineNumber 0.
- */
-function reportsOnLine(lineNumber: number, reports?: api.Diagnostic[]) {
-	if (!reports) {
-		return parametersReport.filter(r => r.range.start.line === lineNumber);
-	}
-	return reports.filter(r => r.range.start.line === lineNumber);
-}
+import * as utils from './ruleUtils';
 
 describe('Parameter tests', () => {
-	beforeAll(async () => {
-		let text = await fs.readFile(testFilePath).then(b => b.toString());
 
-		let pslDocument = new api.PslDocument(parseText(text), text, testFilePath);
-		let ruleSubscriptions = new activate.RuleSubscription(pslDocument);
-		ruleSubscriptions.addMethodRules(new MethodParametersOnNewLine());
-		activate.reportRules(ruleSubscriptions);
-		parametersReport = ruleSubscriptions.diagnostics;
-		
+	let parametersReport: api.Diagnostic[] = [];
+
+	beforeAll(async () => {
+		parametersReport = await utils.getDiagnostics('ZTestParams.PROC', MethodParametersOnNewLine.name);
 	})
 
 	test('No report for no params', () => {
-		expect(reportsOnLine(2).length).toBe(0)
+		expect(utils.diagnosticsOnLine(2, parametersReport).length).toBe(0)
 	})
 
 	test('No report for one param on same line', () => {
-		expect(reportsOnLine(7).length).toBe(0)
+		expect(utils.diagnosticsOnLine(7, parametersReport).length).toBe(0)
 	})
 
 	test('Two reports for two params', () => {
-		expect(reportsOnLine(12).length).toBe(2)
+		expect(utils.diagnosticsOnLine(12, parametersReport).length).toBe(2)
 	})
 
 	test('Catch label', () => {
-		expect(reportsOnLine(17).length).toBe(3)
+		expect(utils.diagnosticsOnLine(17, parametersReport).length).toBe(3)
 	})
 
 	test('Catch no types on params', () => {
-		expect(reportsOnLine(22).length).toBe(4)
+		expect(utils.diagnosticsOnLine(22, parametersReport).length).toBe(4)
 	})
 
 	test('Catch tree', () => {
-		expect(reportsOnLine(27).length).toBe(2)
+		expect(utils.diagnosticsOnLine(27, parametersReport).length).toBe(2)
 	})
 
 	test('Catch tree with empty parens', () => {
-		expect(reportsOnLine(32).length).toBe(2)
+		expect(utils.diagnosticsOnLine(32, parametersReport).length).toBe(2)
 	})
 
 	test('Catch tree with empty parens and commas', () => {
-		expect(reportsOnLine(37).length).toBe(2)
+		expect(utils.diagnosticsOnLine(37, parametersReport).length).toBe(2)
 	})
 })
--- __tests__/runtime-test.ts
+++ __tests__/runtime-test.ts
@@ -1,34 +1,13 @@
 import * as api from '../src/pslLint/api';
-import { parseText } from '../src/parser/parser';
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as activate from '../src/pslLint/activate';
+import * as utils from './ruleUtils';
 import { RuntimeStart } from '../src/pslLint/runtime';
 
-const testFilePath = path.resolve('__tests__', 'files', 'ZRuntime.PROC');
-let runtimeDiagnostics: api.Diagnostic[];
-
-/**
- * 
- * @param reports The reports to filter
- * @param lineNumber Zero-based line number to check, i.e. line 1 of the document is lineNumber 0.
- */
-function reportsOnLine(lineNumber: number, reports?: api.Diagnostic[]) {
-	if (!reports) {
-		return runtimeDiagnostics.filter(r => r.range.start.line === lineNumber);
-	}
-	return reports.filter(r => r.range.start.line === lineNumber);
-}
-
 describe('Parameter tests', () => {
-	beforeAll(async () => {
-		let text = await fs.readFile(testFilePath).then(b => b.toString());
 
-		let pslDocument = new api.PslDocument(parseText(text), text, testFilePath);
-		let ruleSubscriptions = new activate.RuleSubscription(pslDocument);
-		ruleSubscriptions.addMethodRules(new RuntimeStart());
-		activate.reportRules(ruleSubscriptions);
-		runtimeDiagnostics = ruleSubscriptions.diagnostics;
+	let runtimeDiagnostics: api.Diagnostic[] = [];
+
+	beforeAll(async () => {
+		runtimeDiagnostics = await utils.getDiagnostics('ZRuntime.PROC', RuntimeStart.name);
 	})
 
 	test('Diagnostic count', () => {
@@ -36,46 +15,50 @@ describe('Parameter tests', () => {
 	})
 
 	test('No diagnostic first start', () => {
-		expect(reportsOnLine(9)).toMatchObject([]);
+		expect(utils.diagnosticsOnLine(9, runtimeDiagnostics)).toMatchObject([]);
 	})
 	
 	test('One diagnostic second start', () => {
-		let reports = reportsOnLine(13);
+		let reports = utils.diagnosticsOnLine(13, runtimeDiagnostics);
 		expect(reports.length).toBe(1);
 		expect(reports[0].message).toBe(`Declaration "flagged" referenced inside Runtime.start but not in variable list.`);
 	})
 	
 	test('One diagnostic third start', () => {
-		let reports = reportsOnLine(18);
+		let reports = utils.diagnosticsOnLine(18, runtimeDiagnostics);
 		expect(reports.length).toBe(1);
 		expect(reports[0].message).toBe(`Declaration "flagged" referenced inside Runtime.start but not in variable list.`);
 	})
 	
 	test('No diagnostic with comment', () => {
-		expect(reportsOnLine(24)).toMatchObject([]);
+		expect(utils.diagnosticsOnLine(24, runtimeDiagnostics)).toMatchObject([]);
 	})
 
 	test('No diagnostic fifth start', () => {
-		expect(reportsOnLine(29)).toMatchObject([]);
+		expect(utils.diagnosticsOnLine(29, runtimeDiagnostics)).toMatchObject([]);
 	})
 
 	test('Method in middle with new variable', () => {
-		let reports = reportsOnLine(35);
+		let reports = utils.diagnosticsOnLine(35, runtimeDiagnostics);
 		expect(reports.length).toBe(1);
 		expect(reports[0].message).toBe(`Parameter "flaggedParam" referenced inside Runtime.start but not in variable list.`);
 	})
 
 	test('Method in middle with new variable', () => {
-		let reports = reportsOnLine(42);
+		let reports = utils.diagnosticsOnLine(42, runtimeDiagnostics);
+		let diagnostic = reports[0] as api.Diagnostic;
+		let relatedArray = diagnostic.relatedInformation as api.DiagnosticRelatedInformation[];
+		let source = relatedArray[0] as api.DiagnosticRelatedInformation;
+		let reference = relatedArray[1] as api.DiagnosticRelatedInformation;
 		expect(reports.length).toBe(1);
-		expect(reports[0].message).toBe(`Declaration "notFlaggedTwice" referenced inside Runtime.start but not in variable list.`);
-		expect(reports[0].relatedInformation.length).toBe(2);
-		expect(reports[0].relatedInformation[0].message).toBe(`Source of "notFlaggedTwice"`);
-		expect(reports[0].relatedInformation[1].message).toBe(`Reference to "notFlaggedTwice"`);
+		expect(diagnostic.message).toBe(`Declaration "notFlaggedTwice" referenced inside Runtime.start but not in variable list.`);
+		expect(relatedArray.length).toBe(2);
+		expect(source.message).toBe(`Source of "notFlaggedTwice"`);
+		expect(reference.message).toBe(`Reference to "notFlaggedTwice"`);
 	})
 
 	test('No literal', () => {
-		let reports = reportsOnLine(49);
+		let reports = utils.diagnosticsOnLine(49, runtimeDiagnostics);
 		expect(reports.length).toBe(1);
 		expect(reports[0].message).toBe(`Declaration "flagged" referenced inside Runtime.start but not in variable list.`);
 	})
