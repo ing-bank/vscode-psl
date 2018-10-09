96440161d3cc4e6844957fbea325d610647a820f
Alexander Tiplea <Alexander.Tiplea@ing.com>
2018-10-09 16:19:18 +0200

    Add utility module for testing rules

--- __tests__/convention-test.ts
+++ __tests__/convention-test.ts
@@ -1,57 +1,34 @@
 import * as api from '../src/pslLint/api';
-import { parseText } from '../src/parser/parser';
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as activate from '../src/pslLint/activate';
-import { MemberLiteralCase, MemberCamelCase, MemberLength, MemberStartsWithV, MethodStartsWithZ, PropertyStartsWithZ } from '../src/pslLint/elementsConventionChecker';
-
-const testFilePath = path.resolve('__tests__', 'files', 'ZTestConvention.PROC');
-let membersReport: api.Diagnostic[];
-
-/**
- * 
- * @param reports The reports to filter
- * @param lineNumber Zero-based line number to check, i.e. line 1 of the document is lineNumber 0.
- */
-function reportsOnLine(lineNumber: number, reports?: api.Diagnostic[]) {
-	if (!reports) {
-		return membersReport.filter(r => r.range.start.line === lineNumber);
-	}
-	return reports.filter(r => r.range.start.line === lineNumber);
-}
+import * as utils from './ruleUtils';
+import { MemberLiteralCase, MemberCamelCase, MemberLength, MemberStartsWithV } from '../src/pslLint/elementsConventionChecker';
 
 describe('Members tests', () => {
+	let literalDiagnostics: api.Diagnostic[] = [];
+	let camelCaseDiagnostics: api.Diagnostic[] = [];
+	let lengthDiagnostics: api.Diagnostic[] = [];
+	let vDiagnostics: api.Diagnostic[] = [];
+
 	beforeAll(async () => {
-		let text = await fs.readFile(testFilePath).then(b => b.toString());
-
-		let pslDocument = new api.PslDocument(parseText(text), text, testFilePath);
-		let ruleSubscriptions = new activate.RuleSubscription(pslDocument);
-
-		ruleSubscriptions.addMemberRules(new MemberCamelCase());
-		ruleSubscriptions.addMemberRules(new MemberLength());
-		ruleSubscriptions.addMemberRules(new MemberStartsWithV());
-		ruleSubscriptions.addMethodRules(new MethodStartsWithZ());
-		ruleSubscriptions.addMemberRules(new MemberLiteralCase());
-		ruleSubscriptions.addPropertyRules(new PropertyStartsWithZ());
-
-		activate.reportRules(ruleSubscriptions);
-		membersReport = ruleSubscriptions.diagnostics;
-
+		literalDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberLiteralCase.name);
+		camelCaseDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberCamelCase.name);
+		lengthDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberLength.name);
+		vDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberStartsWithV.name);
 	})
 
 	test('Upper case literal report', () => {
-		expect(reportsOnLine(4).length).toBe(1)
+		expect(utils.diagnosticsOnLine(5, literalDiagnostics).length).toBe(1)
 	})
 
 	test('Camel case literal report', () => {
-		expect(reportsOnLine(5).length).toBe(1)
+		expect(utils.diagnosticsOnLine(4, camelCaseDiagnostics).length).toBe(1)
 	})
 
-	test('Starting with z label', () => {
-		expect(reportsOnLine(10).length).toBe(1);
-	})
 
 	test('More than 25 characters', () => {
-		expect(reportsOnLine(14).length).toBe(1);
+		expect(utils.diagnosticsOnLine(14, lengthDiagnostics).length).toBe(1);
+	})
+
+	test('Starts with v', () => {
+		expect(utils.diagnosticsOnLine(23, vDiagnostics).length).toBe(1);
 	})
 })
