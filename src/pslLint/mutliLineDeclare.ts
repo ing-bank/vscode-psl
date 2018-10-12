a084419acc85cc4f8d8b3d8fd6479c0c131aafce
Thirumurugan Shanmugam <thirumurugan.shanmugam@ing.com>
2018-10-12 14:19:55 +0200

    MultiLineDeclare

--- src/pslLint/mutliLineDeclare.ts
+++ src/pslLint/mutliLineDeclare.ts
@@ -1,69 +1,69 @@
-import { MethodRule, PslDocument, Method, Diagnostic, DiagnosticSeverity, getTokens} from "./api";
-import { Member, NON_TYPE_MODIFIERS } from "../parser/parser";
+import { NON_TYPE_MODIFIERS } from '../parser/parser';
+import { Diagnostic, DiagnosticSeverity, getTokens, Method, MethodRule, PslDocument} from './api';
 
 export class MultiLineDeclare implements MethodRule {
-
-    ruleName = MultiLineDeclare.name;
+	ruleName = MultiLineDeclare.name;
 
 	report(pslDocument: PslDocument, method: Method): Diagnostic[] {
-        
-        let diagnostics: Diagnostic[] = [];
-        let prevDeclaration: Member;
 
-        for (let i = 0; i < method.declarations.length; i++) {
-            let reportAllVariables: boolean = false;
-            let curDeclaration = method.declarations[i];
-            let fullLine = pslDocument.getTextAtLine(curDeclaration.id.position.line);
-            
-            if ((((fullLine.indexOf("=") > fullLine.indexOf(",")) && (fullLine.indexOf(",") > -1)) || ((fullLine.indexOf(",") > fullLine.indexOf("=")) && (fullLine.indexOf("=") > -1))) && !(prevDeclaration && prevDeclaration.id.position.line === curDeclaration.id.position.line)) {
-                let conditionOpen: boolean = false;
-                let commaFound: boolean = false;
-                let conditionClose: boolean = false;
-                let typePresent: boolean = false;
+		const diagnostics: Diagnostic[] = [];
+		let prevDeclarationLine: number;
+		let reportAllVariables: boolean = false;
 
-                for (const token of getTokens(fullLine)) {
-                    if (token.isTab()) continue;
-                    if (token.isSpace()) continue;
-                    if (token.isDoubleQuotes()) continue;
-                    if (token.isSlash()) continue;
-                    if ((token.value) === "type") {
-                        typePresent = true;
-                        continue;
-                    }
-                    if (NON_TYPE_MODIFIERS.indexOf(token.value) > -1) {
-                        continue;
-                    }
-                    if (curDeclaration.types.map(t => t.value).indexOf(token.value) > -1) {
-                        continue;
-                    }
-                    if (token.isOpenParen()) { 
-                        conditionOpen = true;
-                        conditionClose = false;
-                        continue;
-                    }
-                    if ((conditionOpen) && (token.isCloseParen())) {
-                        conditionClose = true;
-                        continue;
-                    }
-                    if (token.isComma()) { 
-                        commaFound = true;
-                        continue;
-                    }
-                    if ((commaFound) && (token.isEqualSign()) && (typePresent) && (conditionOpen == conditionClose)) {
-                        conditionOpen =  false;
-                        conditionClose = false;
-                        commaFound = false;
-                        reportAllVariables =  true;
-                    }
-                }
-            }
-            if (reportAllVariables) { 
-                let diag = new Diagnostic(curDeclaration.id.getRange(), `Multiple Declared and Valued variables are in the same line ${curDeclaration.id.value}`, this.ruleName, DiagnosticSeverity.Warning);
-                diag.code = fullLine;
-                diag.source = 'lint';
-                diagnostics.push(diag); 
-            }
-        }
+		for (const declaration of method.declarations) {
+			const curDeclaration = declaration;
+			const fullLine = pslDocument.getTextAtLine(curDeclaration.id.position.line);
+
+			if (!(prevDeclarationLine && prevDeclarationLine === curDeclaration.id.position.line)) {
+				reportAllVariables = false;
+
+				if (!(fullLine.includes('=') && fullLine.includes(','))) continue;
+				let conditionOpen: boolean = false;
+				let commaFound: boolean = false;
+				let conditionClose: boolean = false;
+				let typePresent: boolean = false;
+				for (const token of getTokens(fullLine)) {
+					if (token.isWhiteSpace()) continue;
+					if (token.isDoubleQuotes()) continue;
+					if (token.isBlockCommentInit()) continue;
+					if ((token.value) === 'type') {
+						typePresent = true;
+						continue;
+					}
+					if (NON_TYPE_MODIFIERS.indexOf(token.value) > -1) {
+						continue;
+					}
+					if (curDeclaration.types.map(t => t.value).indexOf(token.value) > -1) {
+						continue;
+					}
+					if (token.isOpenParen()) {
+						conditionOpen = true;
+						conditionClose = false;
+						continue;
+					}
+					if (conditionOpen && token.isCloseParen()) {
+						conditionClose = true;
+						continue;
+					}
+					if (token.isComma()) {
+						commaFound = true;
+						continue;
+					}
+					if (commaFound && token.isEqualSign() && typePresent && conditionOpen === conditionClose) {
+						conditionOpen =  false;
+						conditionClose = false;
+						commaFound = false;
+						reportAllVariables =  true;
+					}
+				}
+			}
+			prevDeclarationLine = curDeclaration.id.position.line;
+			if (reportAllVariables) {
+				const diag = new Diagnostic(curDeclaration.id.getRange(), `Declaration ${curDeclaration.id.value} should be initialized on a new line`, this.ruleName, DiagnosticSeverity.Warning);
+				diag.source = 'lint';
+				diagnostics.push(diag);
+			}
+		}
 		return diagnostics;
-    }
+	}
 }
