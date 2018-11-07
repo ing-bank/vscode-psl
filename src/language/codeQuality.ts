d9ecd24e3e9e496fbc4a927a4ce1fb4be59a5016
Alexander Tiplea <Alexander.Tiplea@ing.com>
2018-11-07 18:43:31 +0100

    codeQuality :lipstick:

--- src/language/codeQuality.ts
+++ src/language/codeQuality.ts
@@ -1,61 +1,65 @@
 import * as vscode from 'vscode';
+import { BATCH_MODE, PSL_MODE, TRIG_MODE } from '../extension';
 import * as parser from '../parser/parser';
 import { getDiagnostics } from '../pslLint/activate';
 import * as api from '../pslLint/api';
-import { PSL_MODE, BATCH_MODE, TRIG_MODE } from '../extension';
-import { setConfig, removeConfig } from '../pslLint/config';
+import { removeConfig, setConfig } from '../pslLint/config';
 import { PSLActionProvider } from './codeAction';
 
-type lintOption = "none" | "all" | "config" | true;
+type lintOption = 'none' | 'all' | 'config' | true;
 
 export async function activate(context: vscode.ExtensionContext) {
 
 	await pslLintConfigurationWatchers(context);
 
-	let lintDiagnostics = vscode.languages.createDiagnosticCollection('psl-lint');
+	const lintDiagnostics = vscode.languages.createDiagnosticCollection('psl-lint');
 	context.subscriptions.push(lintDiagnostics);
 
 	// initial token
 	let tokenSource = new vscode.CancellationTokenSource();
 
 	if (vscode.window.activeTextEditor) {
-		prepareRules(vscode.window.activeTextEditor.document, lintDiagnostics, tokenSource.token)
+		prepareRules(vscode.window.activeTextEditor.document, lintDiagnostics, tokenSource.token);
 	}
 
 	vscode.window.onDidChangeActiveTextEditor(e => {
 		if (!e) return;
-		prepareRules(e.document, lintDiagnostics, tokenSource.token)
-	})
+		prepareRules(e.document, lintDiagnostics, tokenSource.token);
+	});
 
 	vscode.workspace.onDidChangeTextDocument(e => {
 		if (!e) return;
 		tokenSource.cancel();
 		tokenSource = new vscode.CancellationTokenSource();
-		prepareRules(e.document, lintDiagnostics, tokenSource.token)
-	})
+		prepareRules(e.document, lintDiagnostics, tokenSource.token);
+	});
 
 	context.subscriptions.push(
 		vscode.languages.registerCodeActionsProvider(
 			PSL_MODE, new PSLActionProvider(),
-		)
-	)
+		),
+	);
 }
 
 async function pslLintConfigurationWatchers(context: vscode.ExtensionContext) {
-	return Promise.all(vscode.workspace.workspaceFolders.map(workspace => new vscode.RelativePattern(workspace, 'psl-lint.json')).map(async (pattern) => {
-		let watcher = vscode.workspace.createFileSystemWatcher(pattern);
-		context.subscriptions.push(watcher.onDidChange(uri => {
-			setConfig(uri.fsPath);
-		}), watcher.onDidCreate(uri => {
-			setConfig(uri.fsPath);
-		}));
-		watcher.onDidDelete(uri => {
-			removeConfig(uri.fsPath);
-		});
-		let uris = await vscode.workspace.findFiles(pattern);
-		if (!uris.length) return;
-		await setConfig(uris[0].fsPath);
-	}));
+	return Promise.all(
+		vscode.workspace.workspaceFolders
+			.map(workspace => new vscode.RelativePattern(workspace, 'psl-lint.json'))
+			.map(async pattern => {
+				const watcher = vscode.workspace.createFileSystemWatcher(pattern);
+				context.subscriptions.push(watcher.onDidChange(uri => {
+					setConfig(uri.fsPath);
+				}), watcher.onDidCreate(uri => {
+					setConfig(uri.fsPath);
+				}));
+				watcher.onDidDelete(uri => {
+					removeConfig(uri.fsPath);
+				});
+				const uris = await vscode.workspace.findFiles(pattern);
+				if (!uris.length) return;
+				await setConfig(uris[0].fsPath);
+			}),
+	);
 }
 
 export class MemberDiagnostic extends vscode.Diagnostic {
@@ -63,8 +67,11 @@ export class MemberDiagnostic extends vscode.Diagnostic {
 	ruleName: string;
 }
 
-
-function prepareRules(textDocument: vscode.TextDocument, lintDiagnostics: vscode.DiagnosticCollection, cancellationToken: vscode.CancellationToken) {
+function prepareRules(
+	textDocument: vscode.TextDocument,
+	lintDiagnostics: vscode.DiagnosticCollection,
+	cancellationToken: vscode.CancellationToken,
+) {
 	if (!isPSL(textDocument)) return;
 
 	const lintConfigValue: lintOption = vscode.workspace.getConfiguration('psl', textDocument.uri).get('lint');
@@ -82,48 +89,63 @@ function prepareRules(textDocument: vscode.TextDocument, lintDiagnostics: vscode
 		if (!cancellationToken.isCancellationRequested) {
 			lint(textDocument, useConfig, cancellationToken, lintDiagnostics);
 		}
-	})
+	});
 }
 
-function lint(textDocument: vscode.TextDocument, useConfig: boolean, cancellationToken: vscode.CancellationToken, lintDiagnostics: vscode.DiagnosticCollection) {
-	let documentText = textDocument.getText();
-	let pslDocument: api.PslDocument = prepareDocument(documentText, textDocument);
-	let diagnostics = getDiagnostics(pslDocument, useConfig);
-	let memberDiagnostics = transform(diagnostics, textDocument.uri);
+function lint(
+	textDocument: vscode.TextDocument,
+	useConfig: boolean, cancellationToken: vscode.CancellationToken,
+	lintDiagnostics: vscode.DiagnosticCollection,
+) {
+	const documentText = textDocument.getText();
+	const pslDocument: api.PslDocument = prepareDocument(documentText, textDocument);
+	const diagnostics = getDiagnostics(pslDocument, useConfig);
+	const memberDiagnostics = transform(diagnostics, textDocument.uri);
 	process.nextTick(() => {
 		if (!cancellationToken.isCancellationRequested) {
-			lintDiagnostics.set(vscode.Uri.file(textDocument.fileName), memberDiagnostics);
-			vscode.workspace.onDidCloseTextDocument(textDocument => lintDiagnostics.delete(textDocument.uri));
+			lintDiagnostics.set(textDocument.uri, memberDiagnostics);
+			vscode.workspace.onDidCloseTextDocument((textDocument) => {
+				lintDiagnostics.delete(textDocument.uri);
+			});
 		}
 	});
 }
 
 function prepareDocument(documentText: string, textDocument: vscode.TextDocument) {
-	let parsedDocument = parser.parseText(documentText);
-	let getTextAtLine = (n: number) => textDocument.lineAt(n).text;
-	let pslDocument = new api.PslDocument(parsedDocument, documentText, textDocument.uri.fsPath, getTextAtLine);
+	const parsedDocument = parser.parseText(documentText);
+	const getTextAtLine = (n: number) => textDocument.lineAt(n).text;
+	const pslDocument = new api.PslDocument(parsedDocument, documentText, textDocument.uri.fsPath, getTextAtLine);
 	return pslDocument;
 }
 
 function transform(diagnostics: api.Diagnostic[], uri: vscode.Uri): MemberDiagnostic[] {
 	return diagnostics.map(pslLintDiagnostic => {
-		let r = pslLintDiagnostic.range;
-		let vscodeRange = new vscode.Range(r.start.line, r.start.character, r.end.line, r.end.character);
-		let memberDiagnostic = new MemberDiagnostic(vscodeRange, pslLintDiagnostic.message, pslLintDiagnostic.severity);
+		const r = pslLintDiagnostic.range;
+		const vscodeRange = new vscode.Range(r.start.line, r.start.character, r.end.line, r.end.character);
+		const memberDiagnostic = new MemberDiagnostic(vscodeRange, pslLintDiagnostic.message, pslLintDiagnostic.severity);
 		memberDiagnostic.source = pslLintDiagnostic.source;
 		memberDiagnostic.code = pslLintDiagnostic.code;
 		memberDiagnostic.ruleName = pslLintDiagnostic.ruleName;
 		if (pslLintDiagnostic.member) memberDiagnostic.member = pslLintDiagnostic.member;
-		if (pslLintDiagnostic.relatedInformation) memberDiagnostic.relatedInformation = pslLintDiagnostic.relatedInformation.map(x => {
-			return new vscode.DiagnosticRelatedInformation(
-				new vscode.Location(uri, new vscode.Range(x.range.start.line, x.range.start.character, x.range.end.line, x.range.end.character)),
-				x.message
-			)
-		});
+		if (pslLintDiagnostic.relatedInformation) {
+			memberDiagnostic.relatedInformation = pslLintDiagnostic.relatedInformation.map(x => {
+				return new vscode.DiagnosticRelatedInformation(
+					new vscode.Location(uri,
+						new vscode.Range(x.range.start.line,
+							x.range.start.character,
+							x.range.end.line,
+							x.range.end.character,
+						)),
+					x.message,
+				);
+			});
+		}
 		return memberDiagnostic;
-	})
+	});
 }
 
 function isPSL(textDocument: vscode.TextDocument) {
-	return vscode.languages.match(PSL_MODE, textDocument) || vscode.languages.match(BATCH_MODE, textDocument) || vscode.languages.match(TRIG_MODE, textDocument);
+	return vscode.languages.match(PSL_MODE, textDocument) ||
+		vscode.languages.match(BATCH_MODE, textDocument) ||
+		vscode.languages.match(TRIG_MODE, textDocument);
 }
