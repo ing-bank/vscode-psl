77b4a1c631a049c2f0bb4aeed4f75572f0bc5aed
Victoria <me@vika.space>
2018-11-09 21:46:49 +0100

    Style refactoring for convention-test.ts

--- __tests__/convention-test.ts
+++ __tests__/convention-test.ts
@@ -1,6 +1,8 @@
 import * as api from '../src/pslLint/api';
+import {
+	MemberCamelCase, MemberLength,
+	MemberLiteralCase, MemberStartsWithV } from '../src/pslLint/elementsConventionChecker';
 import * as utils from './ruleUtils';
-import { MemberLiteralCase, MemberCamelCase, MemberLength, MemberStartsWithV } from '../src/pslLint/elementsConventionChecker';
 
 describe('Members tests', () => {
 	let literalDiagnostics: api.Diagnostic[] = [];
@@ -13,22 +15,21 @@ describe('Members tests', () => {
 		camelCaseDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberCamelCase.name);
 		lengthDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberLength.name);
 		vDiagnostics = await utils.getDiagnostics('ZTestConvention.PROC', MemberStartsWithV.name);
-	})
+	});
 
 	test('Upper case literal report', () => {
 		expect(utils.diagnosticsOnLine(5, literalDiagnostics).length).toBe(1)
-	})
+	});
 
 	test('Camel case literal report', () => {
 		expect(utils.diagnosticsOnLine(4, camelCaseDiagnostics).length).toBe(1)
-	})
-
+	});
 
 	test('More than 25 characters', () => {
 		expect(utils.diagnosticsOnLine(14, lengthDiagnostics).length).toBe(1);
-	})
+	});
 
 	test('Starts with v', () => {
 		expect(utils.diagnosticsOnLine(23, vDiagnostics).length).toBe(1);
-	})
-})
+	});
+});
