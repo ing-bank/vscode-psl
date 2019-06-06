e8cd4f771e70e2fb19710eb0d1d0b5e55ba31d0b
Alexander Tiplea <Alexander.Tiplea@ing.com>
2019-06-06 15:27:26 +0200

    Enable debugging with status bar

--- src/common/terminal.ts
+++ src/common/terminal.ts
@@ -1,115 +1,44 @@
 import * as vscode from 'vscode';
-import * as environment from './environment';
-
-let terminal: vscode.Terminal | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
-			'psl.launchHostTerminal', launchHostTerminal
-		)
+			'psl.stepIn', stepIn,
+		),
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
-			'psl.stepIn', stepIn
-		)
+			'psl.stepOut', stepOut,
+		),
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
-			'psl.stepOut', stepOut
-		)
+			'psl.stepOver', stepOver,
+		),
 	);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
-			'psl.stepOver', stepOver
-		)
+			'psl.sendToHostTerminal', sendToHostTerminal,
+		),
 	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand(
-			'psl.sendToHostTerminal', sendToHostTerminal
-		)
-	);
-
-	vscode.window.onDidCloseTerminal(cleanTerminal);
-
 	terminalSendSettings();
 
+	configureGtmDebug(context);
 }
 
 async function terminalSendSettings() {
-	let pslTerminalCommands = ['psl.stepIn', 'psl.stepOut', 'psl.stepOver', 'psl.sendToHostTerminal'];
-	let terminalSettings = vscode.workspace.getConfiguration('terminal');
-	let commandsToSkip: string[] | undefined = terminalSettings.get('integrated.commandsToSkipShell');
+	const pslTerminalCommands = ['psl.stepIn', 'psl.stepOut', 'psl.stepOver', 'psl.sendToHostTerminal'];
+	const terminalSettings = vscode.workspace.getConfiguration('terminal');
+	const commandsToSkip: string[] | undefined = terminalSettings.get('integrated.commandsToSkipShell');
 	if (commandsToSkip) {
-		let merged = commandsToSkip.concat(pslTerminalCommands);
-		let filteredMerge = merged.filter((item, pos) => merged.indexOf(item) === pos);
+		const merged = commandsToSkip.concat(pslTerminalCommands);
+		const filteredMerge = merged.filter((item, pos) => merged.indexOf(item) === pos);
 		terminalSettings.update('integrated.commandsToSkipShell', filteredMerge, true);
 	}
-}
-
-function cleanTerminal(someTerminal: vscode.Terminal) {
-	if (someTerminal === terminal) {
-		terminal.dispose();
-		terminal = undefined;
-		vscode.commands.executeCommand('setContext', 'psl.hasHostTerminal', false);
-	}
-}
-
-
-async function getLaunchLogins() {
-	if (!vscode.workspace.workspaceFolders) {
-		throw new Error('No configuration present.');
-	}
-	let logins: environment.LaunchQuickPick[] = []
-	let allEnvironments = await environment.GlobalFile.read();
-	for (let env of allEnvironments.environments) {
-		let login: environment.LaunchQuickPick;
-		try {
-			let label = env.sshLogin ? `${env.sshLogin}@${env.host}` : `${env.host}`
-			login = {env, description: env.name, label};
-			logins.push(login)
-		}
-		catch (e) {
-			continue;
-		}
-	}
-	return logins;
-}
-
-async function launchHostTerminal() {
-	if (!terminal) {
-		let launchLogins = await getLaunchLogins();
-		let choice: environment.LaunchQuickPick;
-		if (launchLogins.length === 1) choice = launchLogins[0];
-		else {
-			choice = await vscode.window.showQuickPick(getLaunchLogins());
-			if (!choice) return;
-		}
-		let sshLogin;
-		let host;
-		try {
-			let envObj = choice.env
-			sshLogin = envObj.sshLogin;
-			if (!sshLogin) {
-				sshLogin = await vscode.window.showInputBox({prompt: 'username'});
-			}
-			if (!sshLogin) return;
-			host = envObj.host;
-		}
-		catch (e) {
-			// alertInvalid(choice.fsPath);
-			vscode.window.showErrorMessage('Invalid environment configuration.')
-			return;
-		}
-		terminal = vscode.window.createTerminal(host);
-		terminal.sendText(`ssh ${sshLogin}@${host}`);
-		vscode.commands.executeCommand('setContext', 'psl.hasHostTerminal', true);
-	}
-	terminal.show();
 }
 
 function stepIn() {
@@ -133,5 +62,43 @@ function terminalSend(text: string) {
 	if (activeTerminal) {
 		activeTerminal.show();
 		activeTerminal.sendText(text, true);
+	}
+}
+
+function configureGtmDebug(context: vscode.ExtensionContext) {
+	const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 901);
+	const commandName = 'psl.setGtmDebug';
+	let gtmDebug = false;
+
+	const set = () => {
+		if (gtmDebug) showInformation(context);
+		statusBar.text = `GT.M Debug ${gtmDebug ? '$(check)' : '$(circle-slash)'}`;
+		vscode.commands.executeCommand('setContext', 'psl.gtmDebug', gtmDebug);
+	};
+
+	set();
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			commandName, () => {
+				gtmDebug = !gtmDebug;
+				set();
+			},
+		),
+	);
+
+	statusBar.command = commandName;
+	statusBar.tooltip = 'GT.M Debugging hotkeys';
+	statusBar.show();
+}
+
+async function showInformation(context: vscode.ExtensionContext) {
+	const doNotShow = context.globalState.get('gtmDebugShow');
+	if (doNotShow) return;
+	const response = await vscode.window.showInformationMessage(
+		'INTO Ctrl+Q | OVER Ctrl+W | OUTOF Ctrl+E',
+		'Do not show again',
+	);
+	if (response) {
+		context.globalState.update('gtmDebugShow', true);
 	}
 }
