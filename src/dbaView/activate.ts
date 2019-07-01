bd3cae57e46aa3938ae0b855f5239a634759d407
Brito Oliveira <rui.brito.oliveira@ing.com>
2019-07-01 17:13:14 +0300

    Vue JS template added

--- src/dbaView/activate.ts
+++ src/dbaView/activate.ts
@@ -25,12 +25,23 @@ async function dbaViewHandler(context: utils.ExtensionCommandContext) {
     
       panel.webview.html = getWebviewContent();
 
-      setTimeout(async _ => {
-        const connection = new MtmConnection();
-        await connection.open(environment.host, environment.port, environment.user, environment.password);
-        const result = await connection.sqlQuery('SELECT FID from DBTBL1');
-        panel.webview.postMessage(result);
-      }, 10000);
+      panel.webview.onDidReceiveMessage(
+        message => {
+          switch (message.command) {
+            case 'alert':
+                async _ => {
+                  const connection = new MtmConnection();
+                  await connection.open(environment.host, environment.port, environment.user, environment.password);
+                  const result = await connection.sqlQuery('SELECT FID from DBTBL1');
+                  vscode.window.showErrorMessage(result);
+                  panel.webview.postMessage(result);
+                }
+              return;
+          }
+        },
+        undefined,
+        context.subscriptions
+      );
     
     
 }
@@ -59,63 +70,180 @@ async function getEnvironmentFromContext(context: utils.ExtensionCommandContext)
 	return utils.getCommandEnvConfigQuickPick(environments);
 }
 
-function getTables() {
-    return {
-        'id':'tables',
-        'data': [
-            {
-                'id': 1,
-                'name': 'TableName 1',
-                'description': 'TableName 1 Description'
-            },
-            {
-                'id': 2,
-                'name': 'TableName 2',
-                'description': 'TableName 2 Description'
-            }
-        ]
-    }
-}
-
 function getWebviewContent() {
     return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>DBA Viewer</title>
-      <style>
-        html, body { height: 100%; padding: 0; margin: 0; }
-        div { width: 50%; height: 50%; float: left; }
-        #table-list { background: #DDD; }
-        #column-list { background: #AAA; }
-        #table-details { background: #777; }
-        #column-details { background: #444; }
-    </style>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DBA Viewer</title>
+        <style>
+          html, body, #app { color: #000; height: 100%; padding: 0; margin: 0; }
+          div.container { width: 50%; height: 50%; float: left; }
+          #table-list { background: #DDD; }
+          #column-list { background: #AAA; }
+          #table-details { background: #777; }
+          #column-details { background: #444; }
+      </style>
+      
+    </head>
+    <body>
+  
+    <div id="app">
+      <div class="container" id="table-list">
+        <form id="search">
+          Search <input name="query" v-model="searchQuery">
+        </form>
+        <demo-grid
+          :tables="tables"
+          :columns="gridColumns"
+          :filter-key="searchQuery">
+        </demo-grid>
+      </div>
+      <div class="container" id="column-list">
+        COLUMN LIST
+      </div>
+      <div class="container" id="table-details">
+        TABLE DETAILS
+      </div>
+      <div class="container" id="column-details">
+        COLUMN DETAILS
+      </div>
+    </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+    <script type="text/x-template" id="grid-template">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="key in columns"
+            @click="sortBy(key)"
+            :class="{ active: sortKey == key }">
+            {{ key | capitalize }}
+            <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+            </span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entry in filteredTables">
+          <td v-for="key in columns">
+            {{entry[key]}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </script>
+    <script>
+    const vscode = acquireVsCodeApi();
+    // register the grid component
+  Vue.component('demo-grid', {
+    template: '#grid-template',
+    props: {
+      tables: Array,
+      columns: Array,
+      filterKey: String
+    },
+    data: function () {
+      var sortOrders = {}
+      this.columns.forEach(function (key) {
+        sortOrders[key] = 1
+      })
+      return {
+        sortKey: '',
+        sortOrders: sortOrders
+      }
+    },
+    computed: {
+      filteredTables: function () {
+        var sortKey = this.sortKey
+        var filterKey = this.filterKey && this.filterKey.toLowerCase()
+        var order = this.sortOrders[sortKey] || 1
+        var tables = this.tables
+        if (filterKey) {
+          tables = tables.filter(function (row) {
+            return Object.keys(row).some(function (key) {
+              return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+            })
+          })
+        }
+        if (sortKey) {
+          tables = tables.slice().sort(function (a, b) {
+            a = a[sortKey]
+            b = b[sortKey]
+            return (a === b ? 0 : a > b ? 1 : -1) * order
+          })
+        }
+        return tables
+      }
+    },
+    filters: {
+      capitalize: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
+    },
+    methods: {
+      sortBy: function (key) {
+        this.sortKey = key
+        this.sortOrders[key] = this.sortOrders[key] * -1
+      }
+    }
+  })
     
-  </head>
-  <body>
-    <div id="table-list">
-    </div>
-    <div id="column-list">
-    </div>
-    <div id="table-details">
-    </div>
-    <div id="column-details">
-    </div>
-  </body>
-  <script>
-    window.addEventListener('message', event => {
-
-        const message = event.data; // The JSON data our extension sent
-        console.log(message);
-        document.getElementById('table-list').textContent(message)
-        // switch (message.id) {
-        //     case 'tables':
-        //         alert(message.data)
-        //         break;
-        // }
+    var app = new Vue({
+      el: '#app',
+      methods:{
+        init() {
+          this.askTables()
+          window.addEventListener('message', event => {
+            this.parseMessage(message)
+        });
+          //Send message to get the tables
+          console.log('Sunt aici!')
+          
+        },
+        parseMessage(message) {
+          const message = event.data; // The JSON data our extension sent
+          switch(message.id) {
+            case 'tables':
+              this.parseTables(message.data)
+              break;
+            case 'columns':
+              break;
+          }
+        },
+        askTables() {
+          vscode.postMessage({
+            command: 'alert',
+            text: 'test'
+          })
+        },
+        parseTables(tables) {
+          document.getElementById('table-details').textContent(tables)
+          console.log(tables);
+        },
+        askColumns(table) {
+          //Send message to get columns from table
+        },
+        parseColumns(columns) {
+          console.log(columns);
+        }
+      },
+      mounted(){
+        this.init();
+      },
+      data: {
+        searchQuery: '',
+        gridColumns: ['name', 'alias', 'description'],
+        tables: [
+          { name: 'Chuck Norris', alias: Infinity , description: 'Testing...'},
+          { name: 'Bruce Lee', alias: 9000 , description: 'Testing...'},
+          { name: 'Jackie Chan', alias: 7000 , description: 'Testing...'},
+          { name: 'Jet Li', alias: 8000 , description: 'Testing...'}
+        ],
+        
+      }
     });
-    </script>
-  </html>`;
+      </script>
+    </html>`;
   }
