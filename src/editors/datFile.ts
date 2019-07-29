399e30ad39945c293a0b98d4d3e54c176e9c2e72
Kandra <Bogdan-Alexandru.Kandra@ing.com>
2019-07-29 16:51:48 +0300

    Updated files as to follow tslint suggestions

--- src/editors/datFile.ts
+++ src/editors/datFile.ts
@@ -1,10 +1,12 @@
-import { commands, ExtensionContext, extensions, Uri, WebviewPanelSerializer, 
-		WebviewPanel, window, workspace } from 'vscode';
+import * as path from 'path';
+import { commands, ExtensionContext, Uri, window, workspace } from 'vscode';
+import DatPreview from './datPreview';
+import { previewManager } from './previewManager';
 import * as utils from './utilityFunctions';
 
 export function activate(context: ExtensionContext) {
 
-    // Preview .DAT File Command
+	// Preview .DAT File Command
 	const datCommand = commands.registerCommand('dat.preview', (arg) => {
 		let resource: Uri;
 		if (arg instanceof Uri) {
@@ -29,26 +31,26 @@ export function activate(context: ExtensionContext) {
 		}
 
 		// Check if the file to be previewed has a header
-		let tableName = path.basename(resource.fsPath, '.DAT').toLowerCase();
+		const tableName = path.basename(resource.fsPath, '.DAT').toLowerCase();
 
 		// Get the Workspace corresponding to the resource
 		let workspacePath = workspace.getWorkspaceFolder(resource).uri.fsPath;
 
 		// Go up the directory structure until the base "Profile_Host" directory is reached
-		if (workspacePath.includes("Profile")) {
-			while (!path.basename(workspacePath).includes("Profile")) {
+		if (workspacePath.includes('Profile')) {
+			while (!path.basename(workspacePath).includes('Profile')) {
 				workspacePath = path.dirname(workspacePath);
 			}
 		}
 
 		// Look in the Profile Host project for a table directory having the same name as the .DAT file
-		let tableDirPath = path.join(workspacePath, 'dataqwik', 'table', tableName);
-		
+		const tableDirPath = path.join(workspacePath, 'dataqwik', 'table', tableName);
+
 		utils.existsReadableFileAsync(tableDirPath)
 			.then(() => {
 				// The table directory exists, proceed with the check
-				let contents : string;
-				let files : string[];
+				let contents: string;
+				let files: string[];
 
 				// Perform parallel reads of the .DAT file and the table directory
 				Promise.all([utils.readFileAsync(resource.fsPath), utils.readDirAsync(tableDirPath)])
@@ -56,32 +58,33 @@ export function activate(context: ExtensionContext) {
 						[contents, files] = results;
 
 						// Build a list containing the values from the first row of the file
-						let separator : string = utils.getLineSeparator(contents);
-						let datFileColumns : string[] = contents.split(separator)[0].split("\t");
+						const separator: string = utils.getLineSeparator(contents);
+						const datFileColumns: string[] = contents.split(separator)[0].split('\t');
 
 						// Build a list containing all .COL files in the table directory
-						let tableColumns : string[] = files.filter(file => file.endsWith(".COL"));
-						tableColumns = tableColumns.map(file => file.split(".")[0].split("-")[1]);
+						let tableColumns: string[] = files.filter(file => file.endsWith('.COL'));
+						tableColumns = tableColumns.map(file => file.split('.')[0].split('-')[1]);
 
 						// Build a list containing all columns missing from the .DAT file header
-						let missingColumns : string[] = [];
-						for (let i = 0; i < tableColumns.length; i++) {
-							if (!datFileColumns.includes(tableColumns[i])) {
-								missingColumns.push(tableColumns[i]);
+						const missingColumns: string[] = [];
+						for (const col of tableColumns) {
+							if (!datFileColumns.includes(col)) {
+								missingColumns.push(col);
 							}
 						}
 
 						// Build an object for tracking whether columns are computed or not
-						let computedObject: object = {};
-						for (let i = 0; i < tableColumns.length; i++) {
-							computedObject[tableColumns[i]] = utils.isComputedColumn(tableDirPath, tableName, tableColumns[i]);
+						const computedObject: object = {};
+						for (const col of tableColumns) {
+							computedObject[col] = utils.isComputedColumn(tableDirPath, tableName, col);
 						}
 
-						let totalUncomputed: number = 0, missingUncomputed: number = 0;
-						let uncomputedColumns: string[] = [];
+						let totalUncomputed: number = 0;
+						let missingUncomputed: number = 0;
+						const uncomputedColumns: string[] = [];
 
 						// Compute the total of uncomputed columns
-						for (let key in computedObject) {
+						for (const key in computedObject) {
 							if (!computedObject[key]) {
 								totalUncomputed++;
 								uncomputedColumns.push(key);
@@ -89,8 +92,8 @@ export function activate(context: ExtensionContext) {
 						}
 
 						// Compute the total of missing uncomputed columns
-						for (let i = 0; i < missingColumns.length; i++) {
-							if (!computedObject[missingColumns[i]]) {
+						for (const col of missingColumns) {
+							if (!computedObject[col]) {
 								missingUncomputed++;
 							}
 						}
@@ -114,11 +117,81 @@ export function activate(context: ExtensionContext) {
 			})
 			.catch((err) => {
 				// The table directory does not exist, inform the user that the check cannot be performed and open the preview
-				window.showWarningMessage(tableName + " could not be found in the Profile Host project. Header existence cannot be analysed");
-
+				window.showWarningMessage(tableName + ' could not be found in the Profile Host project. ' +
+											'Header existence could not be analysed');
 				preview = new DatPreview(context, resource, viewColumn);
 				return preview.webview;
 			});
-
 	});
+
+	// Clear State Command
+	const clearCommand = commands.registerCommand('dat.clearState', () => {
+		const preview = previewManager.active();
+		if (preview) {
+			const key = preview.previewUri.toString();
+			if (preview.storage.get(key)) {
+				preview.storage.update(key, null);
+				preview.refresh(preview.getOptions());
+			}
+		}
+	});
+
+	// Refresh Command
+	const refreshCommand = commands.registerCommand('dat.refresh', () => {
+		const preview = previewManager.active();
+		if (preview) {
+			preview.webview.postMessage({
+				refresh: true,
+			});
+		}
+		preview.refresh(preview.options);
+	});
+
+	// Register the commands
+	context.subscriptions.push(datCommand);
+	context.subscriptions.push(clearCommand);
+	context.subscriptions.push(refreshCommand);
+
+	workspace.onDidSaveTextDocument(document => {
+		if (utils.isDatFile(document)) {
+			const resource = document.uri;
+			const uri = resource.with({
+				scheme: 'dat-preview',
+			});
+			const preview = previewManager.find(uri);
+			if (preview) {
+				preview.refresh(preview.getOptions());
+			}
+		}
+	});
+
+	workspace.onDidChangeTextDocument(args => {
+		if (utils.isDatFile(args.document)) {
+			const resource = args.document.uri;
+			const uri = resource.with({
+				scheme: 'dat-preview',
+			});
+			const preview = previewManager.find(uri);
+			if (preview && args.contentChanges.length > 0) {
+				preview.refresh(preview.getOptions());
+			}
+		}
+	});
+
+	workspace.onDidChangeConfiguration(() => {
+		previewManager.configure();
+	});
+
+	workspace.onDidOpenTextDocument(document => {
+		if (utils.isStdinFile(document)) {
+			commands.executeCommand('dat.preview', document.uri);
+		}
+	});
+
+	if (window.activeTextEditor) {
+		const document = window.activeTextEditor.document;
+		if (utils.isStdinFile(document)) {
+			commands.executeCommand('dat.preview', document.uri);
+		}
+	}
 }
