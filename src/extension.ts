26f62d1c72acc8e4f0cd426dc9c1979fb560d513
Mischa Reitsma <reitsma.mischa@gmail.com>
2022-10-21 11:16:00 +0200

    Support serialized data files

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ export const PSL_MODE: vscode.DocumentFilter = { language: 'psl', scheme: 'file'
 export const BATCH_MODE: vscode.DocumentFilter = { language: 'profileBatch', scheme: 'file' };
 export const TRIG_MODE: vscode.DocumentFilter = { language: 'profileTrigger', scheme: 'file' };
 export const DATA_MODE: vscode.DocumentFilter = { language: 'profileData', scheme: 'file' };
+export const SERIAL_MODE: vscode.DocumentFilter = { language: 'profileSerialData', scheme: 'file'};
 export const TBL_MODE: vscode.DocumentFilter = { language: 'profileTable', scheme: 'file' };
 export const COL_MODE: vscode.DocumentFilter = { language: 'profileColumn', scheme: 'file' };
 
